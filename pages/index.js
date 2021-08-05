754994fa23b2e301a794133b8a059226de9bd480
earth-r <88467838+earth-r@users.noreply.github.com>
2021-08-05 17:43:40 -0400

    deploy

--- pages/index.js
+++ pages/index.js
@@ -33,8 +33,10 @@ const Index = () => {
       </Head>
       <div>
       { videos ? videos.map((video, i) => (
+        {console.log(video.videos)}
         <div key={i}>
           {video['videos'].map((vid, i) => (
+            {cconsole.log(vid.embed)}
               <div key={i} className="w-screen h-screen" dangerouslySetInnerHtml={{ _html: vid.embed }}></div>
           ))}
         </div>
